b2d57a0cfcdc4ab34d6e93a5202e6f55dd4ccceb
Dmitry Momot <mail@dmomot.com>
2019-03-10 04:42:20 +0200

    checker

---
+++ client.go
@@ -0,0 +1,35 @@
+package consulclient
+
+import "github.com/hashicorp/consul/api"
+
+type (
+	// Client strucure
+	Client struct {
+		api *api.Client
+	}
+
+	// Config of listener
+	Config struct {
+		ID                string            `json:",omitempty"`
+		Name              string            `json:",omitempty"`
+		Tags              []string          `json:",omitempty"`
+		Port              int               `json:",omitempty"`
+		Address           string            `json:",omitempty"`
+		EnableTagOverride bool              `json:",omitempty"`
+		Meta              map[string]string `json:",omitempty"`
+		Weights           *api.AgentWeights `json:",omitempty"`
+		Check             *api.AgentServiceCheck
+		Checks            api.AgentServiceChecks
+	}
+)
+
+// NewClient creates a new consul client
+func NewClient(addr string) (*Client, error) {
+	config := api.DefaultConfig()
+	config.Address = addr
+	c, err := api.NewClient(config)
+	if err != nil {
+		return nil, err
+	}
+	return &Client{api: c}, nil
+}
--- listener.go
+++ listener.go
@@ -2,58 +2,15 @@ package consulclient
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/gosimple/slug"
 	"github.com/hashicorp/consul/api"
 	"github.com/segmentio/ksuid"
 )
 
-type (
-	// Client strucure
-	Client struct {
-		api *api.Client
-	}
-
-	// Config of listener
-	Config struct {
-		ID                string            `json:",omitempty"`
-		Name              string            `json:",omitempty"`
-		Tags              []string          `json:",omitempty"`
-		Port              int               `json:",omitempty"`
-		Address           string            `json:",omitempty"`
-		EnableTagOverride bool              `json:",omitempty"`
-		Meta              map[string]string `json:",omitempty"`
-		Weights           *api.AgentWeights `json:",omitempty"`
-		Check             *api.AgentServiceCheck
-		Checks            api.AgentServiceChecks
-	}
-)
-
-// NewClient creates a new consul client
-func NewClient(addr string) (*Client, error) {
-	config := api.DefaultConfig()
-	config.Address = addr
-	c, err := api.NewClient(config)
-	if err != nil {
-		return nil, err
-	}
-	return &Client{api: c}, nil
-}
-
 // NewListener register new listener in consul with default configuration
 func (c *Client) NewListener(name, addr string, port int, tags ...string) (string, error) {
 	cnf, err := getDefaultConfig(name, addr, port, tags...)
-	if err != nil {
-		return "", err
-	}
-	return cnf.ID, c.api.Agent().ServiceRegister(mapConfig(cnf))
-}
-
-// NewHTTPListenerWithHealthcheck register new listener in consul
-// with default configuration and custom healthcheck
-func (c *Client) NewHTTPListenerWithHealthcheck(name, addr string, port int, hc string, tags ...string) (string, error) {
-	cnf, err := getDefaultConfigWithHTTPHealthcheck(name, addr, port, hc, tags...)
 	if err != nil {
 		return "", err
 	}
@@ -96,34 +53,4 @@ func getDefaultConfig(name, addr string, port int, tags ...string) (*Config, err
 		Port:    port,
 		Tags:    tags,
 	}, nil
-}
-
-func getDefaultConfigWithHTTPHealthcheck(name, addr string, port int, hc string, tags ...string) (*Config, error) {
-	cnf, err := getDefaultConfig(name, addr, port, tags...)
-	if err != nil {
-		return nil, err
-	}
-
-	checkID := fmt.Sprintf("%s-healthcheck-%s", name, ksuid.New().String())
-
-	cnf.Check = &api.AgentServiceCheck{
-		CheckID:  checkID,
-		Name:     checkID,
-		Interval: "10s",
-		Timeout:  "1s",
-		Method:   "GET",
-		Header: map[string][]string{
-			"Content-Type": []string{"application/json"},
-			"Accept":       []string{"application/json"},
-		},
-		HTTP: fmt.Sprintf("%s/health", addr),
-	}
-
-	if strings.Contains(hc, "://") {
-		cnf.Check.HTTP = hc
-	} else if len(hc) > 0 {
-		cnf.Check.HTTP = fmt.Sprintf("http://%s:%d/%s", addr, port, strings.TrimLeft(hc, "/"))
-	}
-
-	return cnf, nil
 }
