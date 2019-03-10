618a81b3366b6f272ad95a63d75e53359271382f
Dmitry Momot <mail@dmomot.com>
2019-03-10 05:04:11 +0200

    health check

--- listener.go
+++ listener.go
@@ -9,38 +9,6 @@ import (
 	"github.com/segmentio/ksuid"
 )
 
-type (
-	// Client strucure
-	Client struct {
-		api *api.Client
-	}
-
-	// Config of listener
-	Config struct {
-		ID                string            `json:",omitempty"`
-		Name              string            `json:",omitempty"`
-		Tags              []string          `json:",omitempty"`
-		Port              int               `json:",omitempty"`
-		Address           string            `json:",omitempty"`
-		EnableTagOverride bool              `json:",omitempty"`
-		Meta              map[string]string `json:",omitempty"`
-		Weights           *api.AgentWeights `json:",omitempty"`
-		Check             *api.AgentServiceCheck
-		Checks            api.AgentServiceChecks
-	}
-)
-
-// NewClient creates a new consul client
-func NewClient(addr string) (*Client, error) {
-	config := api.DefaultConfig()
-	config.Address = addr
-	c, err := api.NewClient(config)
-	if err != nil {
-		return nil, err
-	}
-	return &Client{api: c}, nil
-}
-
 // NewListener register new listener in consul with default configuration
 func (c *Client) NewListener(name, addr string, port int, tags ...string) (string, error) {
 	cnf, err := getDefaultConfig(name, addr, port, tags...)
@@ -114,7 +82,8 @@ func getDefaultConfigWithHTTPHealthcheck(name, addr string, port int, hc string,
 			"Content-Type": []string{"application/json"},
 			"Accept":       []string{"application/json"},
 		},
-		HTTP: fmt.Sprintf("%s/health", addr),
+		HTTP:   fmt.Sprintf("http://%s:%d/health", addr, port),
+		Status: "warning",
 	}
 
 	if strings.Contains(hc, "://") {
